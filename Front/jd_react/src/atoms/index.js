be04cf4071050746a4055c197ed4e37b412e4231
wwd <243912281@qq.com>
2022-07-22 17:49:18 +0800

    创建一个atoms文件夹以便于状态管理

---
+++ Front/jd_react/src/atoms/index.js
@@ -0,0 +1,6 @@
+import { atom } from 'recoil';
+
+export const nameTokenState = atom({
+  key: 'nameTokenState',
+  default: '',
+});
--- Front/jd_react/src/index.js
+++ Front/jd_react/src/index.js
@@ -4,15 +4,15 @@ import './index.css';
 // import App from './App';
 import App from './router/index';
 import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RecoilRoot>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </RecoilRoot>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();

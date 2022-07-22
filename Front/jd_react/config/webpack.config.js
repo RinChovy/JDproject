df7ad40aaf7a769c1a259290b25761dd93625434
wwd <243912281@qq.com>
2022-07-22 16:57:50 +0800

    加入less - 组件

--- Front/jd_react/config/webpack.config.js
+++ Front/jd_react/config/webpack.config.js
@@ -71,6 +71,9 @@ const cssRegex = /\.css$/;
 const cssModuleRegex = /\.module\.css$/;
 const sassRegex = /\.(scss|sass)$/;
 const sassModuleRegex = /\.module\.(scss|sass)$/;
+//配置less
+const lessRegex = /\.less$/;
+const lessModuleRegex = /\.module\.less$/;
 
 const hasJsxRuntime = (() => {
   if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
@@ -119,6 +122,9 @@ module.exports = function (webpackEnv) {
       {
         loader: require.resolve('css-loader'),
         options: cssOptions,
+      },
+      {
+        loader: require.resolve('less-loader'),
       },
       {
         // Options for PostCSS as we reference these options twice
@@ -222,12 +228,13 @@ module.exports = function (webpackEnv) {
       publicPath: paths.publicUrlOrPath,
       // Point sourcemap entries to original disk location (format as URL on Windows)
       devtoolModuleFilenameTemplate: isEnvProduction
-        ? info =>
+        ? (info) =>
             path
               .relative(paths.appSrc, info.absoluteResourcePath)
               .replace(/\\/g, '/')
         : isEnvDevelopment &&
-          (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
+          ((info) =>
+            path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
     },
     cache: {
       type: 'filesystem',
@@ -237,7 +244,7 @@ module.exports = function (webpackEnv) {
       buildDependencies: {
         defaultWebpack: ['webpack/lib/'],
         config: [__filename],
-        tsconfig: [paths.appTsConfig, paths.appJsConfig].filter(f =>
+        tsconfig: [paths.appTsConfig, paths.appJsConfig].filter((f) =>
           fs.existsSync(f)
         ),
       },
@@ -307,8 +314,8 @@ module.exports = function (webpackEnv) {
       // `web` extension prefixes have been added for better support
       // for React Native Web.
       extensions: paths.moduleFileExtensions
-        .map(ext => `.${ext}`)
-        .filter(ext => useTypeScript || !ext.includes('ts')),
+        .map((ext) => `.${ext}`)
+        .filter((ext) => useTypeScript || !ext.includes('ts')),
       alias: {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
@@ -419,7 +426,7 @@ module.exports = function (webpackEnv) {
                     },
                   ],
                 ],
-                
+
                 plugins: [
                   isEnvDevelopment &&
                     shouldUseReactRefresh &&
@@ -453,7 +460,7 @@ module.exports = function (webpackEnv) {
                 cacheDirectory: true,
                 // See #6846 for context on why cacheCompression is disabled
                 cacheCompression: false,
-                
+
                 // Babel sourcemaps are needed for debugging into node_modules
                 // code.  Without the options below, debuggers like VSCode
                 // show incorrect code and set breakpoints on the wrong lines.
@@ -542,6 +549,33 @@ module.exports = function (webpackEnv) {
                 },
                 'sass-loader'
               ),
+            },
+            {
+              test: lessRegex,
+              exclude: lessModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction
+                    ? shouldUseSourceMap
+                    : isEnvDevelopment,
+                },
+                'less-loader'
+              ),
+              sideEffects: true,
+            },
+            {
+              test: lessModuleRegex,
+              use: getStyleLoaders(
+                {
+                  importLoaders: 2,
+                  sourceMap: isEnvProduction
+                    ? shouldUseSourceMap
+                    : isEnvDevelopment,
+                },
+                'less-loader'
+              ),
+              sideEffects: true,
             },
             // "file" loader makes sure those assets get served by WebpackDevServer.
             // When you `import` an asset, you get its (virtual) filename.
@@ -643,7 +677,7 @@ module.exports = function (webpackEnv) {
             return manifest;
           }, seed);
           const entrypointFiles = entrypoints.main.filter(
-            fileName => !fileName.endsWith('.map')
+            (fileName) => !fileName.endsWith('.map')
           );
 
           return {
--- Front/jd_react/src/page/Login/index.jsx
+++ Front/jd_react/src/page/Login/index.jsx
@@ -1,27 +1,92 @@
 import React from 'react';
-import { Button, Space } from 'antd-mobile';
-import style from './private.less';
+import { Button, Picker, Grid, Form, Input } from 'antd-mobile';
+import { HandPayCircleOutline } from 'antd-mobile-icons';
+import './private.less';
 
 class Login extends React.Component {
   state = {
     list: [],
+    selectValue: '',
+    visible: false,
+    jsonDate: [
+      [
+        { label: '金儿系统', value: '金儿身份' },
+        { label: '多儿系统', value: '多儿身份' },
+        { label: '我他吗什么身份', value: '我他吗什么身份' },
+      ],
+    ],
   };
 
   componentDidMount() {}
 
+  // 提交方法
+  handleSubmit = () => {
+    alert('家人们提交方法我还没写');
+  };
   render() {
+    const { visible, jsonDate, selectValue } = this.state;
     return (
       <div>
-        <div className={style.divTop}>123</div>
-        <p>家人们我害怕</p>
-        <Space wrap>
-          <Button color="primary" fill="solid">
-            Solid
-          </Button>
-          <Button color="primary" fill="solid">
-            Solid
-          </Button>
-        </Space>
+        <div className="divTop">
+          <HandPayCircleOutline fontSize={40} color="#00b578" />
+          <span>家人们我害怕系统</span>
+        </div>
+        <Form className="form" onFinish={this.handleSubmit}>
+          <div className="divMiddle">
+            <Form.Item name="name" label="用户名" rules={[{ required: true }]}>
+              <Input placeholder="请输入用户名" />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              label="密码"
+              rules={[{ required: true }]}
+            >
+              <Input placeholder="请输入密码" />
+            </Form.Item>
+
+            <Form.Item
+              name="select"
+              label="身份"
+              rules={[{ required: true }]}
+              onClick={() => {
+                this.setState({
+                  visible: true,
+                });
+              }}
+            >
+              <Input value={selectValue} placeholder="请选择身份" readOnly />
+              <Picker
+                columns={jsonDate}
+                visible={visible}
+                onClose={() => {
+                  this.setState({
+                    visible: false,
+                  });
+                }}
+                value={selectValue}
+                onConfirm={(v) => {
+                  this.setState({
+                    selectValue: v,
+                  });
+                }}
+              />
+            </Form.Item>
+          </div>
+          <div className="divBottom">
+            <Button
+              block
+              size="large"
+              color="success"
+              style={{ width: '90%' }}
+              type="submit"
+            >
+              登录
+            </Button>
+          </div>
+        </Form>
+        <div className="divBottom">
+          <p>测试版本0.1</p>
+        </div>
       </div>
     );
   }

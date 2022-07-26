dc5c51396e63ef04d6b9c62ebba7eed3380d23f1
jinxiaohao <549903818@qq.com>
2022-07-26 10:04:07 +0800

    修改目录层级

---
+++ Back/jd-config/src/main/java/com/example/jdconfig/JdConfigApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdConfigApplication.class, args);
+    }
+
+}
